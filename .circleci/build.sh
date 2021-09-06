60e1e77357f1b4cff2a35f3e376a4659aba89013
xulei <leo.xu@shoufuyou.com>
2021-09-06 11:40:40 +0800

    test 6666

---
+++ .circleci/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eo pipefail
+
+IMAGE_PREFIX='1142051133'
+STABLE_TAG='0.3'
+
+DOCKERHUB_USERNAME='1142051133' 
+
+TAG="${STABLE_TAG}.${CIRCLE_BUILD_NUM}"
+ROOT_DIR="$(pwd)"
+SVC_DIR="${ROOT_DIR}"
+
+cd $SVC_DIR
+#docker login -u $DOCKERHUB_USERNAME --password-stdin $DOCKERHUB_PASSWORD
+docker login -u $DOCKERHUB_USERNAME  --password-stdin < ./my_password
+
+UNTAGGED_IMAGE=$(echo "${IMAGE_PREFIX}/cricile" | sed -e 's/_/-/g' -e 's/-service//g')
+STABLE_IMAGE="${UNTAGGED_IMAGE}:${STABLE_TAG}"
+IMAGE="${UNTAGGED_IMAGE}:${TAG}"
+echo "image: $IMAGE"
+echo "stable image: ${STABLE_IMAGE}"
+docker build -t "$IMAGE" .
+docker tag "${IMAGE}" "${STABLE_IMAGE}"
+docker push "${IMAGE}"
