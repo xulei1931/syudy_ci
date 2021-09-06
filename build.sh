a29a1c0a7f7898047c97d7bbbc85023f5a4c3be4
xulei <leo.xu@shoufuyou.com>
2021-09-06 09:57:06 +0800

    test images

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eo pipefail
+
+IMAGE_PREFIX='x1x1'
+STABLE_TAG='0.3'
+
+TAG="${STABLE_TAG}.${CIRCLE_BUILD_NUM}"
+ROOT_DIR="$(pwd)"
+SVC_DIR="${ROOT_DIR}"
+cd $SVC_DIR
+#docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
+docker login -u 1142051133 -p xulei123456
+
+UNTAGGED_IMAGE=$(echo "${IMAGE_PREFIX}/delinkcious-1" | sed -e 's/_/-/g' -e 's/-service//g')
+STABLE_IMAGE="${UNTAGGED_IMAGE}:${STABLE_TAG}"
+IMAGE="${UNTAGGED_IMAGE}:${TAG}"
+echo "image: $IMAGE"
+echo "stable image: ${STABLE_IMAGE}"
+docker build -t "$IMAGE" .
+docker tag "${IMAGE}" "${STABLE_IMAGE}"
+docker push "${IMAGE}"
+docker push "${STABLE_IMAGE}"
