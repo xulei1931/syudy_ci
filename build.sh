e61a288f0b7f26c295a0f9544b62c75ad4e4d8d9
xulei <leo.xu@shoufuyou.com>
2021-09-06 10:09:09 +0800

    test2  2  2 2

--- build.sh
+++ build.sh
@@ -5,12 +5,16 @@ set -eo pipefail
 IMAGE_PREFIX='x1x1'
 STABLE_TAG='0.3'
 
+DOCKERHUB_USERNAME='1142051133' 
+DOCKERHUB_PASSWORD='xulei123456'
+
 TAG="${STABLE_TAG}.${CIRCLE_BUILD_NUM}"
 ROOT_DIR="$(pwd)"
 SVC_DIR="${ROOT_DIR}"
+
 cd $SVC_DIR
-#docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
-docker login -u 1142051133 -p xulei123456
+docker login -u $DOCKERHUB_USERNAME --password-stdin $DOCKERHUB_PASSWORD
+# docker login -u 1142051133 -p xulei123456
 
 UNTAGGED_IMAGE=$(echo "${IMAGE_PREFIX}/delinkcious-1" | sed -e 's/_/-/g' -e 's/-service//g')
 STABLE_IMAGE="${UNTAGGED_IMAGE}:${STABLE_TAG}"
